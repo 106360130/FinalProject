921d6a58933fcb4c335966f9c2ddd7cd404de4cf
106360130 <bs25017@gmail.com>
2019-12-31 22:26:10 +0800

    有加入Database的功能了，成能測試成功

--- app/src/main/java/com/example/foodemerge/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/foodemerge/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.foodemerge.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.foodemerge.Database.DatabaseForm;
+import com.example.foodemerge.Database.DatabaseFunction;
 import com.example.foodemerge.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -20,6 +25,21 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        //測試資料庫提取資料，測試成功
+        ArrayList<DatabaseForm> dialog_foods = DatabaseFunction.getInstance().getDatabase();  //取得剛剛儲存的資料
+        Log.e("dialog_foods ; " , String.format("%d" , dialog_foods.size()));
+        //DatabaseForm dailog_food = dialog_foods.get(0);  //取第一筆資料
+        DatabaseForm test_database2 = dialog_foods.get(dialog_foods.size()-1);  //取最後一筆資料
+
+        Log.e("TEST_DATEBASE2 : ", "food name : " + test_database2.food_neme);
+        Log.e("TEST_DATEBASE2 : ", "food cals : " + test_database2.food_cals);
+        Log.e("TEST_DATEBASE2 : ", "food protein : " + test_database2.food_protein);
+        Log.e("TEST_DATEBASE2 : ", "food fat : " + test_database2.food_fat);
+        Log.e("TEST_DATEBASE2 : ", "food carbs : " + test_database2.food_carbs);
+        //測試資料庫提取資料，測試成功
+
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
