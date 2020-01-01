8883cd904f231d60bb5c2240c9ea664137abc84b
arual14 <laura14dic@hotmail.com>
2020-01-01 21:59:56 +0800

    加了gettext,改了layout

--- app/src/main/java/com/example/foodemerge/ui/shopping_list/ShoppingListFragment.java
+++ app/src/main/java/com/example/foodemerge/ui/shopping_list/ShoppingListFragment.java
@@ -128,9 +128,9 @@ public class ShoppingListFragment extends Fragment {
                             try {
                                 //增加食物的功能
                                 DatabaseForm shopping_list1 = new DatabaseForm();
-                                shopping_list1.food_name = ed_food.toString();
+                                shopping_list1.food_name = ed_food.getText().toString();
                                 Log.e("FOOD_NAME : ", shopping_list1.food_name);
-                                items.add(shopping_list1.food_name);
+                                items.add("名字: "+ shopping_list1.food_name);
                                 Toast.makeText(getActivity(), "新增食物" + ed_food.getText().toString(), /*+ "      價格" + ed_price.getText().toString(),*/ Toast.LENGTH_SHORT).show();
 
                                 ed_food.setText("");
