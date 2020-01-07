e564e834ae111b09fd17701a7e8ebdea0889d076
106360130 <bs25017@gmail.com>
2020-01-07 16:57:04 +0800

    有圖表資料出來，成功，但有點小bug，資料要全部都有才行

--- app/src/main/java/com/example/foodemerge/MainActivity.java
+++ app/src/main/java/com/example/foodemerge/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        /*
         //DATABASE_FOOD_INFO，存取資料
         DatabaseForm test_database = new DatabaseForm();
         test_database.food_name = "chicken";
@@ -68,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
         Log.e("TEST_DATEBASE2 : ", "food fat : " + test_database2.food_fat);
         Log.e("TEST_DATEBASE2 : ", "food carbs : " + test_database2.food_carbs);
         //DATABASE_FOOD_INFO，讀取資料
+        */
+
 
 
 
